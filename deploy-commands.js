dbc8caa8a6e6842e676c9c0d754935c529259b20
sjsjsnsnsjna <babapiro20091@gmail.com>
2026-06-30 17:52:14 +0300

    Add deploy-commands.js

---
+++ deploy-commands.js
@@ -0,0 +1,29 @@
+'use strict';
+require('dotenv').config();
+const { REST, Routes } = require('discord.js');
+const fs = require('fs');
+const path = require('path');
+
+const commands = [];
+const commandFiles = fs.readdirSync(path.join(__dirname, 'commands')).filter(f => f.endsWith('.js'));
+
+for (const file of commandFiles) {
+  const command = require(path.join(__dirname, 'commands', file));
+  commands.push(command.data.toJSON());
+}
+
+const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
+
+(async () => {
+  try {
+    console.log(`[deploy] ${commands.length} komut kaydediliyor...`);
+    await rest.put(
+      Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+      { body: commands },
+    );
+    console.log('[deploy] Komutlar başarıyla kaydedildi.');
+  } catch (e) {
+    console.error('[deploy] Hata:', e);
+    process.exit(1);
+  }
+})();
